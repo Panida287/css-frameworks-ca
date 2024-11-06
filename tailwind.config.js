e6dbbca59715acc4e1e355c99cfcaeb8da2e1eb7
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-11-06 16:00:27 +0100

    Chore: config tailwind
    - added screen sizes
    - added colors
    - added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,html}"
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        primary: '#E85D04',
+        darkGrey: '#2E2E2E',
+        lightGrey: '#F1F1F1'
+      },
+      fontFamily: {
+        headerText: ['Poppins', 'sans-serif'],
+        pText: ['Roboto', 'sans-serif']
+      }
+    }
   },
   plugins: [],
-}
-
+};
