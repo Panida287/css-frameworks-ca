3a38aa0ea14eb40e0654a859ddd51f64e408eb20
Panida Paethanom <143201084+Panida287@users.noreply.github.com>
2024-11-12 00:46:53 +0100

    feat: created profile page
    - added html element to profile page
    - styled with tailwind css
    - made responsive across screen sizes
    - added functionality to hamburger menu

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 import './src/css/styles.css'
 
+const btn = document.getElementById('hamburger-icon');
+const menu = document.getElementById('mobile-menu');
+
+btn.addEventListener('click', ()=> {
+    menu.classList.toggle('flex')
+    menu.classList.toggle('hidden')
+})
